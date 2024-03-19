269fa880afbc7a5ddff8625cd9132456a1959de9
kaansimsek <kaansimsek.dev@gmail.com>
2024-03-19 16:37:00 +0300

    Initial Commit

---
+++ WeightManagement/WeightManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightManagementApp.swift
+//  WeightManagement
+//
+//  Created by Kaan Şimşek on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeightManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
